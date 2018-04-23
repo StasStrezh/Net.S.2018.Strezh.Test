a074cfdf67dee65a2ace11d4ddd31e2dd69e7347
Stas Strezh <37235564+StasStrezh@users.noreply.github.com>
2018-04-23 11:34:34 +0300

    Update Program.cs

--- NET.W.2017.Strezh.Test/Task3.Console/Program.cs
+++ NET.W.2017.Strezh.Test/Task3.Console/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task3.Console
+namespace Task3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Stock stock = new Stock();
+            Bank bank = new Bank("Bank of America", stock);
+            Broker broker = new Broker("Rokefeller", stock);
+            stock.Market();
+            Console.ReadKey();
         }
     }
 }
