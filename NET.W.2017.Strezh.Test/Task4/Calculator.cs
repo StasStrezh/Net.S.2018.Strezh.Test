fe7e565165e585a7ab2ba41ffc01b7a5e3f2af2d
Stas Strezh <37235564+StasStrezh@users.noreply.github.com>
2018-04-23 12:12:03 +0300

    Add files via upload

---
+++ NET.W.2017.Strezh.Test/Task4/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4
+{
+    public class Calculator
+    {
+        public double CalculateAverage(List<double> values, IMethod method)
+        {
+            if (values == null)
+                throw new ArgumentNullException();
+
+            if (method == null)
+                throw new ArgumentNullException();
+
+            double result = method.Calculate(values);
+            return result;
+        }
+    }
+}
